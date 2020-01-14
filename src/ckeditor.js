ad28b31febaffa917d8c24f0f8bd85228b27e853
卫宣 <weixuan75@163.com>
2020-01-14 13:58:14 +0800

    初始化

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -15,11 +15,14 @@ import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
+
 import Indent from '@ckeditor/ckeditor5-indent/src/indent';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
@@ -35,6 +38,7 @@ import Font from '@ckeditor/ckeditor5-font/src/font';
 import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
 import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
 import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount';
+import StandardEditingMode from '@ckeditor/ckeditor5-restricted-editing/src/standardeditingmode';
 
 export default class ClassicEditor extends ClassicEditorBase {
 }
@@ -55,11 +59,9 @@ ClassicEditor.builtinPlugins = [
 	CKFinder,
 	EasyImage,
 	Heading,
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
+	Image, ImageToolbar, ImageCaption, ImageStyle, ImageResize,
 	ImageUpload,
+	StandardEditingMode,//受限制编辑
 	Indent,
 	Link,
 	List,
@@ -74,62 +76,34 @@ ClassicEditor.builtinPlugins = [
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
-	fontSize: {
-		options: [
-			'tiny',
-			'default',
-			'big',
-			'huge'
-		]
-	},fontColor: {
-		colors: [
-			{
-				color: 'hsl(0, 0%, 0%)',
-				label: 'Black'
-			},
-			{
-				color: 'hsl(0, 0%, 30%)',
-				label: 'Dim grey'
-			},
-			{
-				color: 'hsl(0, 0%, 60%)',
-				label: 'Grey'
-			},
-			{
-				color: 'hsl(0, 0%, 90%)',
-				label: 'Light grey'
-			},
-			{
-				color: 'hsl(0, 0%, 100%)',
-				label: 'White',
-				hasBorder: true
-			},
-
-			// ...
-		]},fontBackgroundColor: {
-		colors: [
-			{
-				color: 'hsl(0, 75%, 60%)',
-				label: 'Red'
-			},
-			{
-				color: 'hsl(30, 75%, 60%)',
-				label: 'Orange'
-			},
-			{
-				color: 'hsl(60, 75%, 60%)',
-				label: 'Yellow'
-			},
-			{
-				color: 'hsl(90, 75%, 60%)',
-				label: 'Light green'
-			},
-			{
-				color: 'hsl(120, 75%, 60%)',
-				label: 'Green'
-			},
-
-			// ...
+	codeBlock: {
+		languages: [
+			{language: 'bash', label: 'Bash/Shell'},
+			{language: 'json', label: 'json'},
+			{language: 'java', label: 'Java'},
+			{language: 'pl', label: 'Perl'},
+			{language: 'php', label: 'PHP'},
+			{language: 'erlang', label: 'Erlang'},
+			{language: 'goLang', label: 'goLang'},
+			{language: 'ps', label: 'PowerShell'},
+			{language: 'python', label: 'Python'},
+			{language: 'sql', label: 'SQL'},
+			{language: 'html', label: 'HTML'},
+			{language: 'xml', label: 'XML'},
+			{language: 'css', label: 'CSS'},
+			{language: 'js', label: 'JavaScript'},
+			{language: 'cpp', label: 'C/C++'},
+			{language: 'c#', label: 'C#'},
+			{language: 'plain', label: 'Plain Text'},
+			{language: 'cf', label: 'ColdFusion'},
+			{language: 'as3', label: 'ActionScript 3'},
+			{language: 'delphi', label: 'Delphi'},
+			{language: 'diff', label: 'Diff'},
+			{language: 'groovy', label: 'Groovy'},
+			{language: 'jfx', label: 'JavaFX'},
+			{language: 'ruby', label: 'Ruby'},
+			{language: 'scala', label: 'Scala'},
+			{language: 'vb', label: 'Visual Basic'}
 		]
 	},
 	toolbar: {
@@ -150,31 +124,18 @@ ClassicEditor.defaultConfig = {
 			'numberedList',
 			'highlight',
 			'|',
+			'codeBlock',
 			'indent',
 			'outdent',
+			'restrictedEditingException',
 			'|',
+			'imageTextAlternative', '|', 'imageStyle:full', 'imageStyle:side',
 			'imageUpload',
 			'blockQuote',
 			'insertTable',
 			'mediaEmbed',
-			'codeBlock',
 			'undo',
 			'redo'
-		]
-	},
-	image: {
-		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
-			'|',
-			'imageTextAlternative'
-		]
-	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
 		]
 	},
 	//此值必须与webpack.config.js中定义的语言保持同步。
